bc16d1d2518679969228f22d31f60f6d0dc86d0a
Dylan Brandenburg <brandenburgd@Gmail.com>
2018-05-29 16:21:12 -0500

    finished practice

--- practice.js
+++ practice.js
@@ -14,7 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+var me = {
+  name: 'Dylan',
+  age: 26,
+}
+
+me.name
 
 
 
@@ -25,7 +30,14 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+var favoriteThings = {
+  band: 'Ed Sheeran',
+  food: 'Sushi',
+  person: 'Leslie', 
+  book: 'Cat and the hat', 
+  movie: 'Step-Brothers', 
+  holiday: 'Christmas', 
+}
 
 
 
@@ -33,7 +45,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Audi";
+favoriteThings.brand = "Apple";
 
 
 
@@ -41,7 +54,8 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 
 
@@ -55,7 +69,14 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+var backPack = {
+
+}
+
+var item = "firstPocket"
+
+backPack["firstPocket"] = 'chapstick';
+backPack.color = "blue";
 
 
 
@@ -63,7 +84,7 @@
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+// alert(backPack)
 
 
 
@@ -73,8 +94,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
-
+console.log(backPack)
 
 
 ////////// PROBLEM 4 //////////
@@ -96,7 +116,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Tyler S. McGinnis';
+user2.email= 'tyler.mcginnis@devmounta.in'
 
 
 
@@ -108,7 +129,6 @@ var user2 = {
   Create an empty object called methodCollection.
 */
 
-//Code Here
 
 
 
@@ -117,7 +137,9 @@ var user2 = {
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
 */
 
-//Code Here
+
+
+
 
 
 
